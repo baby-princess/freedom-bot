bf43553797acbd2add301d4ed797ca830946a2d1
baby-princess <fluffybaby01@protonmail.com>
2019-04-07 21:03:39 +0000

    added man page, and copyleft information

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ bot.on("message", async message => {
 		if(command === `${prefix}invite`){
 			message.channel.send("invite me to your server with this link:\n https://discordapp.com/oauth2/authorize?client_id=564523507480920085&scope=bot");
 		}
+	if(command === `${prefix}sourcecode`) {
+		message.channel.send("my source code is here:\n	https://github.com/baby-princess/freedom-bot.git");
+	}
+	if(command === `${prefix}host`) {
+		message.channel.send("I am running on the following system:\nOS:\tFreeBSD 12.0-RELEASE amd64\nCPU:\tIntel Celeron G465\nGPU:\t2nd Generation Core Processor Family Integrated Graphics Controller\nRAM:4GB total");
+	}
+	if (command === `${prefix}copyleft`) {
+		message.channel.send("https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Copyleft.svg/1200px-Copyleft.svg.png\nwhat is copyleft?\nit is the opisite to copyright, which aims to protect ideas, copyleft is where ideas are free to everyone, and in the public domain, and abides by the freedom of information\n\nso why bother with copyleft?\nthe right to information is a basic human right, without copyleft, this right would be nonexistant, without copyleftests the world would be locked away behind a paywal;, and constantly spying on you to ensure you do not break the copright agreement\ndo people actully use copyleft??\nYes copyleft is actully very widley adaopted, since the begining of time, as a means of sharing ideas, some notable copylefters are the GNU project (though they hold copyright in the GNU name, but the agreement is to copyleft, and disallow properierty programs to be wrote from there software)\nthe TOR project\nanyone who pirates anything ;)\n\nwhow how can i get started with copyleft?\nfor starters rip up your copyright agreements, then start sharing your ideas, and allowing collbarative efforts to make those ideas be better or work etc, allow the world to remix your idea, to formulate new ideas, start sharing scamatics, and source code, recipies etc. and finally do NOT ever confrom to copyright ideas again, you are a free being, not one that needs paywalls, most see doors, locks and walls, i see puzzles\nhttps://www.hooktube.com/watch?v=2EnX0vACj4Q");
+	}
+	if(command === `${prefix}man`) {
+		message.channel.send("\nFREEDOM-BOT(1)\t\tManual Pager Utils\t\tFREEDOM-BOT(1)\nNAME\nfreedom-bot - A discord bot which repects free software and the GNU standards\n\nSYNOPSIS\nfreedom-bot [hello-world] | [invite] | [sourcecode] | [host] | [copyleft] | [man]\nDESCRIPTION\nfreedom-bot is a discord bot that respects the UNIX philosphy.\n\ndefault prefix is GNU/ \nEXAMPLES\nfreedom-bot hello-world\nruns the hello-world command\nfreedom-bot host\n displays information about the host system\n0.0.1\t\t2019-04-07\t\tFREEDOM-BOT(1)")
+	}
 });
 
 
